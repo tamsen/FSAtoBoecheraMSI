86228c3c9824af85d9da357ec81b4161f308e2b1
tamsen <tamsen.dunn@gmail.com>
2022-12-08 17:03:22 -0800

    adding recalling code

--- unit_tests/test_query.py
+++ unit_tests/test_query.py
@@ -12,15 +12,15 @@ class TestQuery(unittest.TestCase):
 
     # 'http://sites.biology.duke.edu/windhamlab/files/TD21RP25_SearchResults_ASscore.xls'" >
     # URL1 = "https://sites.biology.duke.edu/windhamlab/cgi-bin/Daddy_finder_batch.py"
-    #URL3 = "https://sites.biology.duke.edu/windhamlab/files/BD1200CNTRL_SearchResults_ASscore.xls"
-    #URL2 = "http://sites.biology.duke.edu/windhamlab/files/TD21RP25_SearchResults_ASscore.xls"
+    # URL3 = "https://sites.biology.duke.edu/windhamlab/files/BD1200CNTRL_SearchResults_ASscore.xls"
+    # URL2 = "http://sites.biology.duke.edu/windhamlab/files/TD21RP25_SearchResults_ASscore.xls"
 
     def setUp(self):
 
         if not (os.path.exists(test_globals.GLOBAL_test_output_dir)):
             os.makedirs(test_globals.GLOBAL_test_output_dir)
 
-        out_folder=os.path.join(test_globals.GLOBAL_test_output_dir, "test_BMW_query")
+        out_folder = os.path.join(test_globals.GLOBAL_test_output_dir, "test_BMW_query")
         if not (os.path.exists(out_folder)):
             os.makedirs(out_folder)
 
@@ -34,26 +34,83 @@ class TestQuery(unittest.TestCase):
         URL2 = "http://sites.biology.duke.edu/windhamlab/files/TD21RP25_SearchResults_ASscore.xls"
         results2 = query_file.submit_plain_query(URL2)
 
-        out_file=os.path.join(test_globals.GLOBAL_test_output_dir, "test_BMW_query","TD21RP25_BatchQuery_Ouput.txt")
+        out_file = os.path.join(test_globals.GLOBAL_test_output_dir, "test_BMW_query", "TD21RP25_BatchQuery_Ouput.txt")
 
         with open(out_file, 'wb') as f:
             f.write(results2)
 
         with open(out_file, 'r') as f:
-           lines = f.readlines()
+            lines = f.readlines()
 
         print("third line:" + lines[2])
 
+    def test_adjusted_calls(self):
+
+        #in_dir = "/home/tamsen/Data/Eton/by_site/Bennetville/" + \
+        #          "FSA_to_microsat_script_results__v1.0.0.6__02_12_2022_11_55_23"
+        #in_file = os.path.join(in_dir, "BatchQueryForRecall_02_12_2022_11_56_30.csv")
+
+        in_dir = "/home/tamsen/Data/Eton/by_site/DanaGibbs2021/" + \
+                  "FSA_to_microsat_script_results__v1.0.0.6__02_12_2022_13_03_41"
+        in_file = os.path.join(in_dir, "BatchQuery_ForRecall_02_12_2022_13_07_32.tsv")
+
+        in_dir = "/home/tamsen/Data/Eton/by_site/EmmaLake/" + \
+                  "FSA_to_microsat_script_results__v1.0.0.6__02_12_2022_12_21_07"
+        in_file = os.path.join(in_dir, "BatchQuery_RecallBySample_02_12_2022_12_24_12.tsv")
+
+        #in_dir = "/home/tamsen/Data/Eton/by_site/RedPeak2021/" + \
+        # FSA_to_microsat_script_results__v1.0.0.6__02_12_2022_14_53_07"
+        in_file = os.path.join(in_dir, "BatchQuery_RecallBySample_02_12_2022_14_57_49.tsv")
+
+        in_dir = "/home/tamsen/Data/Eton/by_site/GardiskyLake2021/" + \
+                    "FSA_to_microsat_script_results__v1.0.0.6__02_12_2022_12_30_36"
+        in_file = os.path.join(in_dir, "BatchQuery_RecallBySample_02_12_2022_12_33_03.tsv")
+
+
+        in_dir = "/home/tamsen/Data/Eton/by_site/GardiskyLake2022/" + \
+                    "FSA_to_microsat_script_results__v1.0.0.6__02_12_2022_11_56_43"
+        in_file = os.path.join(in_dir, "BatchQuery_RecallBySample_02_12_2022_11_57_32.tsv")
+
+        in_dir = "/home/tamsen/Data/Eton/by_site/MonoPass/" + \
+                    "FSA_to_microsat_script_results__v1.0.0.6__08_12_2022_12_30_42"
+        in_file = os.path.join(in_dir, "BatchQuery_RecallBySample_08_12_2022_12_31_12.tsv")
+
+        in_dir = "/home/tamsen/Data/Eton/by_site/MountConness/" + \
+                    "FSA_to_microsat_script_results__v1.0.0.6__02_12_2022_12_24_51"
+        in_file = os.path.join(in_dir, "BatchQuery_RecallBySample_02_12_2022_12_25_01.tsv")
+
+        in_dir = "/home/tamsen/Data/Eton/by_site/MountLassen/" + \
+                    "FSA_to_microsat_script_results__v1.0.0.6__02_12_2022_12_33_25"
+        in_file = os.path.join(in_dir, "BatchQuery_RecallBySample_02_12_2022_12_34_15.tsv")
+
+        in_dir = "/home/tamsen/Data/Eton/by_site/RubyLake2020and2021/" + \
+                 "FSA_to_microsat_script_results__v1.0.0.6__02_12_2022_12_11_36"
+        in_file = os.path.join(in_dir, "BatchQuery_RecallBySample_02_12_2022_12_19_11.tsv")
+
+
+        in_dir = "/home/tamsen/Data/Eton/by_site/SaddlebagLake2021/" + \
+                 "FSA_to_microsat_script_results__v1.0.0.6__02_12_2022_12_25_03"
+        in_file = os.path.join(in_dir, "BatchQuery_RecallBySample_02_12_2022_12_29_27.tsv")
+
+        URL1 = "https://sites.biology.duke.edu/windhamlab/cgi-bin/Search_database_batch.py"
+        results1 = query_file.submit_file_query(in_file, URL1)
+        out_file = os.path.join(in_dir, "Recall_Output.html")
+
+
+
+        with open(out_file, 'wb') as f:
+            f.write(results1)
+
     def test_query(self):
 
-         #cgi - bin / Search_database_byAllele.py
-        #Search_database_batch.py
+        # cgi - bin / Search_database_byAllele.py
+        # Search_database_batch.py
         URL1 = "https://sites.biology.duke.edu/windhamlab/cgi-bin/Search_database_batch.py"
-        in_file=os.path.join(test_globals.GLOBAL_test_input_dir, "test_BMW_query", "BatchQuery_b.tsv")
-        results1 = query_file.submit_file_query(in_file,URL1)
+        in_file = os.path.join(test_globals.GLOBAL_test_input_dir, "test_BMW_query", "BatchQuery_b.tsv")
+        results1 = query_file.submit_file_query(in_file, URL1)
         tables = pd.read_html(results1)  # Returns list of all tables on page
 
-        out_file=os.path.join(test_globals.GLOBAL_test_output_dir, "test_BMW_query","BatchQuery_Ouput.html")
+        out_file = os.path.join(test_globals.GLOBAL_test_output_dir, "test_BMW_query", "BatchQuery_Ouput.html")
 
         with open(out_file, 'wb') as f:
             f.write(results1)
