e65e1da06e6c26c369336fbc1244c16dc0acce4c
tamsen <tamsen.dunn@gmail.com>
2023-02-22 18:10:26 -0800

    added some housekeeping scripts

--- unit_tests/test_housekeeping.py
+++ unit_tests/test_housekeeping.py
@@ -5,6 +5,28 @@ import unittest
 
 class test_housekeeping(unittest.TestCase):
 
+    def test_move_and_rename_TD_files(self):
+
+        data_path_in = "/home/tamsen/Data/Eton/Frag_Order_16830/a_raw"
+        data_path_out = "/home/tamsen/Data/Eton/Frag_Order_16830/a_renamed"
+        all_files = os.listdir(data_path_in)
+        for f in all_files:
+                print("\tfile:" + f)
+                original_file = os.path.join(data_path_in, f)
+
+                if "a-" in f:
+                    new_name = f.replace("a-", "-")
+
+
+                new_file = os.path.join(data_path_out, new_name)
+                print("original file: " + original_file)
+                print("destination file: " + new_file)
+                shutil.copy(original_file, new_file)
+                print("copy complete")
+
+        self.assertEqual(True, True)
+
+
     def move_and_rename_MW_files(self):
 
         data_path="/home/tamsen/Data/MWRawFSAData/BMW_Calibration_Batch2"
